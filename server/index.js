c18f74114e2353c521d0ccfffe70c63fa5992bc0
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-08-12 14:26:24 +0300

    hide server port

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 
 import express from "express"
 import pay from "./Routes/PayentRoute.js"
-
+import { config } from "dotenv";
+config()
 const app = express()
-
 app.use("/api/mpesa",pay)
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
 console.log("server running");
+
+
   });
